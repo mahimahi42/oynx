df3b96c6b609cd190d73a91c23913d3b8493b66c
Bryce Davis <bad1128@gmail.com>
2014-04-03 11:09:23 -0400

    Implement Setup.welcome() method

---
+++ setup.rb
@@ -0,0 +1,11 @@
+module Setup
+	def Setup.welcome()
+		puts "Welcome to Oynx!"
+		puts "This tool will simplify your life as a web guru."
+		puts "\n\n"
+	end
+
+	def Setup.get_site_info()
+		puts "First, let's get some info about the site."
+	end
+end
